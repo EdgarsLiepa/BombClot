6fcd34b2cfb121d663d0fc7bd6e3fae15c176e19
GustavsOzolins <35406816+GustavsOzolins@users.noreply.github.com>
2018-01-19 03:35:52 +0200

    Add files via upload
    Mani darbi man liekas

---
+++ resources/views/home.blade.php
@@ -0,0 +1,109 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>BomboClot</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css"
+    integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/bombs.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <header class="masthead">
+
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+          <a class="navbar-brand" href="#">Bomba Clot</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="top100">Top 100</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="news">News</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="Genius.com">Related</a>
+              </li>
+
+              </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+          </div>
+        </nav>
+
+      </header>
+
+      <main role="main">
+
+        <!-- Jumbotron -->
+        <div class="jumbotron">
+          <h2>Latest News</h2>
+          <img class="img-fluid rounded" src="https://www.straightofficial.com/wp-content/uploads/2017/04/bannerOrig1450848406-1.jpg" alt=""> </img>
+          <h2>How A New Generation Of Chicago Artists Redefined Rap</h2>
+          <p><a class="btn btn-lg btn-success" href="article1" role="button">Read More about it here!</a></p>
+
+        <div class="row">
+          <div class="col-lg-4">
+            <img src="http://static.djbooth.net/pics-features/future-drugs-drowning.jpg" width="250" height="200" frameborder="0" allowtransparency="true"> </img>
+            <h3>How Lean Became Hip-Hop’s Dangerous Addiction</h3>
+
+            <h6>From DJ Screw to Lil Pump, Houston’s influential drink found its way onto the charts and into hip-hop tragedies.</h6>
+            <p><a class="btn btn-primary" href="article2" role="button">Read More &raquo;</a></p>
+          </div>
+          <div class="col-lg-4">
+            <img src="https://static.vibe.com/files/2017/06/Mumble-Rap-VIBE-Essay-1496698088-1828x1000.jpg" width="250" height="200" frameborder="0" allowtransparency="true"> </img>
+
+            <h3>Breaking Down The Origins Of Mumble Rap</h3>
+            <h6>Lyrical dexterity sometimes takes a backseat to melody and flow, but it’s not the first time.</h6>
+            <p><a class="btn btn-primary" href="article3" role="button">Read More &raquo;</a></p>
+          </div>
+          <div class="col-lg-4">
+            <img src="https://images.genius.com/9069fe91bf764445b01cfcbcb7f59ec9.1000x563x1.jpg" width="250" height="200" frameborder="0" allowtransparency="true"> </img>
+
+            <h3>Should Stealing Flows Matter In Hip-Hop?</h3>
+            <h6>As hip-hop has grown, co-opting flows has become far more common. Calling rappers out for biting flows is a touchy subject and can end up in violence. </h6>
+            <p><a class="btn btn-primary" href="article4" role="button">Read More &raquo;</a></p>
+          </div>
+        </div>
+      </div>
+
+      </main>
+
+      <!-- Site footer -->
+      <footer class="footer">
+        <p>&copy; Urin DragonTM</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../../../dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
